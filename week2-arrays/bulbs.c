9759a9a664880975f12558a6f5e867a3956f59a9
Maimi <103867210+maimaimi@users.noreply.github.com>
2023-04-25 13:15:58 -0500

    Adding week 2, Arrays!

---
+++ week2-arrays/bulbs.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO(Text-->ASCII-->Binary)
+    string message = get_string("Message: ");
+
+    for (int i = 0, n = strlen(message); i < n; i++)
+    {
+        int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int decimal = message[i];
+        int j = 0;
+
+        while (decimal > 0)
+        {
+            binary[j] = decimal % 2; //modulo
+            decimal = decimal / 2;
+            j++;
+        }
+        //in reverse order
+        for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
---
+++ week2-arrays/readability.c
@@ -0,0 +1,54 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    float L = (float)letters / (float)words * 100;
+    float S = (float)sentences / (float)words * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 16)
+    {
+        printf("Grade %i\n", index);
+    }
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+
+}
